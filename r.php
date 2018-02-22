e1855eae19f9132d3e46e4e60a3bc5e7e068292c
h4mid007 <h4mid007@users.noreply.github.com>
2018-02-22 14:38:35 +0330

    Update r.php

--- r.php
+++ r.php
@@ -10,9 +10,15 @@ if (isset($tg->callback_query)) {
 $mid = $tg->message->message_id;
 $uid = $tg->message->from->id;
 //file_get_contents("https://api.telegram.org/$botapi/sendMessage?chat_id=32709704&text=Ok");
-
+/*
 if ($uid !== 32709704 OR $uid !== 340809697) {
 	exit;
+}
+*/
+if ($uid !== 32709704) {
+	if ($uid !== 340809697) {
+		exit;
+	}
 }
 $m = urldecode($tg->message->text);
 if (isset($tg->callback_query)) {
